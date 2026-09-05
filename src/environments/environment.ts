85b807d6af0f1f4eedd9f1d3dc2954edfa8712ee
TIADRA432 <166173794+TIADRA432@users.noreply.github.com>
2026-09-05 06:27:16 +0000

    feat: configure Supabase environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,8 @@
 export const environment = {
   production: false,
   apiUrl: '/api',
-  firebase: {
-    apiKey: "FIREBASE_KEY",
-    authDomain: "fibeg-archive.firebaseapp.com",
-    projectId: "fibeg-archive",
-    storageBucket: "fibeg-archive.firebasestorage.app",
-    messagingSenderId: "310702492888",
-    appId: "1:310702492888:web:32f89a5771d49c484edbad"
+  supabase: {
+    url: 'https://pesraxtkhkruiipjkrty.supabase.co',
+    publishableKey: 'sb_publishable_YbO3AJ1C-Z3YMYYP_YrsLQ_9KxS_M9w'
   }
 };
