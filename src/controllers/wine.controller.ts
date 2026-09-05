0edaad3a7203197cf83a76b11d0ed86efc4d045e
TIADRA432 <166173794+TIADRA432@users.noreply.github.com>
2026-09-05 06:42:21 +0000

    fix: normalize wine route parameter types

--- src/controllers/wine.controller.ts
+++ src/controllers/wine.controller.ts
@@ -1,38 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { getCollection, addDoc, updateDoc, deleteDoc } from '../services/db.service';
-
-export const getWines = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const data = await getCollection('wineItems');
-    res.json(data.sort((a: any, b: any) => a.displayOrder - b.displayOrder));
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const createWine = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const newDoc = await addDoc('wineItems', req.body);
-    res.status(201).json(newDoc);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const updateWine = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const updatedDoc = await updateDoc('wineItems', req.params.id, req.body);
-    res.json(updatedDoc);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const deleteWine = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    await deleteDoc('wineItems', req.params.id);
-    res.json({ success: true });
-  } catch (error) {
-    next(error);
-  }
-};
+export const getWines = async (req: Request, res: Response, next: NextFunction) => { try { const data=await getCollection('wineItems'); res.json(data.sort((a:any,b:any)=>a.displayOrder-b.displayOrder)); } catch(error){ next(error); } };
+export const createWine = async (req: Request, res: Response, next: NextFunction) => { try { res.status(201).json(await addDoc('wineItems',req.body)); } catch(error){ next(error); } };
+export const updateWine = async (req: Request, res: Response, next: NextFunction) => { try { res.json(await updateDoc('wineItems',String(req.params.id),req.body)); } catch(error){ next(error); } };
+export const deleteWine = async (req: Request, res: Response, next: NextFunction) => { try { await deleteDoc('wineItems',String(req.params.id)); res.json({success:true}); } catch(error){ next(error); } };
